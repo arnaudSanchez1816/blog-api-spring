c2efd238ed588416bc76f7806a5007441aafa84b
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-02 01:23:27 +0200

    Specify rolename when signin up a new user

--- services/api/src/signup/signupService.js
+++ services/api/src/signup/signupService.js
@@ -1,7 +1,12 @@
 import userService from "../users/usersService.js"
 
 export const signupUser = async ({ email, name, password }) => {
-    const newUser = await userService.createUser({ email, name, password })
+    const newUser = await userService.createUser({
+        email,
+        name,
+        password,
+        roleName: "user",
+    })
     return newUser
 }
 
