49f6229df390af7fedd825f7e056296706c86c62
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-20 01:12:26 +0200

    Fetch tags from api instead of placeholder

--- apps/web/src/api/tags.js
+++ apps/web/src/api/tags.js
@@ -1,63 +1,12 @@
-const tags = [
-    {
-        id: 1,
-        name: "JavaScript",
-        slug: "js",
-    },
-    {
-        id: 2,
-        name: "React",
-        slug: "react",
-    },
-    {
-        id: 3,
-        name: "Express",
-        slug: "express",
-    },
-    {
-        id: 4,
-        name: "Frontend",
-        slug: "frontend",
-    },
-    {
-        id: 5,
-        name: "Backend",
-        slug: "backend",
-    },
-    {
-        id: 6,
-        name: "CSS",
-        slug: "css",
-    },
-    {
-        id: 7,
-        name: "TypeScript",
-        slug: "ts",
-    },
-    {
-        id: 8,
-        name: "Tools",
-        slug: "tools",
-    },
-    {
-        id: 9,
-        name: "Postgres",
-        slug: "postgres",
-    },
-    {
-        id: 10,
-        name: "Vite",
-        slug: "vite",
-    },
-]
-
 export const getTags = async () => {
-    const fetchTags = await new Promise((resolve) => {
-        setTimeout(() => resolve(tags), 150)
-    })
-
-    return {
-        count: fetchTags.length,
-        results: fetchTags,
+    const apiUrl = import.meta.env.VITE_API_URL
+    const url = new URL(`./tags`, apiUrl)
+    const response = await fetch(url, { mode: "cors" })
+    if (!response.ok) {
+        throw response
     }
+
+    const tags = await response.json()
+
+    return tags
 }
