466b4abbf9cc85607ddf2160b9923bca4a134159
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-09-24 19:38:39 +0200

    Add a new resusable ThreeColumnLayout component SearchLayout is now based on ThreeColumnLayout

--- apps/cms/src/layouts/SearchLayout.jsx
+++ apps/cms/src/layouts/SearchLayout.jsx
@@ -10,6 +10,7 @@ import {
 import { useCallback, useEffect, useRef, useState } from "react"
 import Tag from "@repo/ui/components/Tag"
 import SearchIcon from "@repo/ui/components/Icons/SearchIcon"
+import ThreeColumnLayout from "./ThreeColumnLayout"
 
 const getTags = async () => {
     const apiUrl = import.meta.env.VITE_API_URL
@@ -57,60 +58,62 @@ export default function SearchLayout() {
     }, [])
 
     return (
-        <div className="mx-auto pt-6 xl:grid xl:grid-cols-[1fr_minmax(auto,65ch)_1fr] xl:justify-center xl:gap-x-16">
-            <div className="m-auto max-w-prose xl:m-0 xl:justify-self-end">
-                {title && (
-                    <h1 className="text-2xl font-medium md:text-3xl">
-                        {title}
-                    </h1>
-                )}
-                {leftContent && <>{leftContent}</>}
-            </div>
-            <div className="m-auto mt-8 w-full max-w-prose xl:mt-0">
-                <Outlet context={[leftContent, setLeftContent]} />
-            </div>
-            <aside className="mx-auto mt-24 max-w-prose xl:mx-0 xl:mt-0 xl:max-w-xs">
-                <Form
-                    className="flex flex-nowrap gap-4"
-                    method="GET"
-                    action="/search"
-                    onSubmit={onSearchSubmit}
-                    key={location.key}
-                >
-                    <Input
-                        classNames={{
-                            input: "text-sm",
-                            inputWrapper:
-                                "font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 h-[40px]",
-                            label: "text-lg font-medium",
-                        }}
-                        size="sm"
-                        startContent={<SearchIcon size={18} />}
-                        type="search"
-                        aria-label="search posts"
-                        label="Find a post"
-                        labelPlacement="outside-top"
-                        name="q"
-                        defaultValue={q}
-                        ref={searchInputRef}
-                    />
-                    <Button
-                        radius="sm"
-                        className="bg-default-900 text-content1 h-[40px] self-end"
-                        type="submit"
+        <ThreeColumnLayout
+            left={
+                <>
+                    {title && (
+                        <h1 className="text-2xl font-medium md:text-3xl">
+                            {title}
+                        </h1>
+                    )}
+                    {leftContent && <>{leftContent}</>}
+                </>
+            }
+            center={<Outlet context={[leftContent, setLeftContent]} />}
+            right={
+                <aside>
+                    <Form
+                        className="flex flex-nowrap gap-4"
+                        method="GET"
+                        action="/search"
+                        onSubmit={onSearchSubmit}
+                        key={location.key}
                     >
-                        Search
-                    </Button>
-                </Form>
-                <div className="mt-8">
-                    <h3 className="text-lg font-medium">All tags</h3>
-                    <div className="mt-6 flex max-w-full flex-wrap gap-2">
-                        {tags.map((tag) => (
-                            <Tag key={tag.id} tag={tag} />
-                        ))}
+                        <Input
+                            classNames={{
+                                input: "text-sm",
+                                inputWrapper:
+                                    "font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 h-[40px]",
+                                label: "text-lg font-medium",
+                            }}
+                            size="sm"
+                            startContent={<SearchIcon size={18} />}
+                            type="search"
+                            aria-label="search posts"
+                            label="Find a post"
+                            labelPlacement="outside-top"
+                            name="q"
+                            defaultValue={q}
+                            ref={searchInputRef}
+                        />
+                        <Button
+                            radius="sm"
+                            className="bg-default-900 text-content1 h-[40px] self-end"
+                            type="submit"
+                        >
+                            Search
+                        </Button>
+                    </Form>
+                    <div className="mt-8">
+                        <h3 className="text-lg font-medium">All tags</h3>
+                        <div className="mt-6 flex max-w-full flex-wrap gap-2">
+                            {tags.map((tag) => (
+                                <Tag key={tag.id} tag={tag} />
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </aside>
-        </div>
+                </aside>
+            }
+        />
     )
 }
---
+++ apps/cms/src/layouts/ThreeColumnLayout.jsx
@@ -0,0 +1,15 @@
+export default function ThreeColumnLayout({ left, center, right }) {
+    return (
+        <div className="mx-auto pt-6 xl:grid xl:grid-cols-[1fr_minmax(auto,65ch)_1fr] xl:justify-center xl:gap-x-16">
+            <div className="m-auto max-w-prose xl:m-0 xl:justify-self-end">
+                {left}
+            </div>
+            <div className="m-auto mt-8 w-full max-w-prose xl:mt-0">
+                {center}
+            </div>
+            <div className="mx-auto mt-24 max-w-prose xl:mx-0 xl:mt-0 xl:max-w-xs">
+                {right}
+            </div>
+        </div>
+    )
+}
