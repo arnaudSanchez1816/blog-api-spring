af8afdddc9f0cefe6a42fea7e36119cd65c541e2
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-11-22 04:08:55 +0100

    Add some tests for /users POST route

---
+++ services/api/src/tests/users.test.js
@@ -0,0 +1,133 @@
+import { describe, expect, it, beforeEach } from "vitest"
+import { api, v1Api } from "./helpers/supertest.js"
+import prisma from "./helpers/prisma.js"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
+import _ from "lodash"
+
+function generateAccessToken({ id, name, email }) {
+    const token = jwt.sign(
+        {
+            sub: id,
+            name,
+            email,
+        },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            expiresIn: "10 minutes",
+        }
+    )
+
+    return token
+}
+
+describe("/users", () => {
+    const adminEmail = "admin@email.com"
+    const userEmail = "user@email.com"
+    const password = "password"
+    let adminUser
+    let regularUser
+
+    beforeEach(async () => {
+        const hashedPassword = bcryptjs.hashSync(
+            password,
+            +process.env.PASSWORD_SALT_LENGTH
+        )
+        const [admin, regular] = await prisma.$transaction([
+            prisma.user.create({
+                data: {
+                    email: adminEmail,
+                    name: "admin",
+                    password: hashedPassword,
+                    roles: {
+                        connect: { name: "admin" },
+                    },
+                },
+            }),
+            prisma.user.create({
+                data: {
+                    email: userEmail,
+                    name: "user",
+                    password: hashedPassword,
+                    roles: {
+                        connect: { name: "user" },
+                    },
+                },
+            }),
+        ])
+        adminUser = admin
+        regularUser = regular
+    })
+
+    describe("[POST] /users", () => {
+        it("should create a user and respond with 201 and send the created user details", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "Password10",
+                    passwordConfirmation: "Password10",
+                    role: "user",
+                })
+
+            const newUser = await prisma.user.findFirst({
+                where: {
+                    email: newUserEmail,
+                },
+            })
+
+            expect(newUser).not.toBeNull()
+            expect(status).toBe(201)
+            expect(body).toStrictEqual(_.omit(newUser, ["password"]))
+        })
+
+        it("should respond 401 if no authorization header is provided", async () => {
+            const newUserEmail = "newUser@email.com"
+            const { status } = await api.post(v1Api("/users")).send({
+                name: "newUser",
+                email: newUserEmail,
+                password: "Password10",
+                passwordConfirmation: "Password10",
+                role: "user",
+            })
+
+            expect(status).toBe(401)
+        })
+
+        it("should respond 401 if the authorization header is invalid", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const newUserData = {
+                name: "newUser",
+                email: newUserEmail,
+                password: "Password10",
+                passwordConfirmation: "Password10",
+                role: "user",
+            }
+            let { status, body } = await api
+                .post(v1Api("/users"))
+                .set("Authorization", `beeeerer ${token}`)
+                .send(newUserData)
+
+            expect(status).toBe(401)
+            ;({ status, body } = await api
+                .post(v1Api("/users"))
+                .set("Authorization", `Bearer${token}`)
+                .send(newUserData))
+
+            expect(status).toBe(401)
+            ;({ status, body } = await api
+                .post(v1Api("/users"))
+                .set("Authorization", `${token}`)
+                .send(newUserData))
+
+            expect(status).toBe(401)
+        })
+    })
+})
