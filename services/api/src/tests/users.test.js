b10a4fffbe71a5d1efaea17ed9c155702227b792
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-11-25 01:50:48 +0100

    Add body fields confirmation tests for POST /users route

--- services/api/src/tests/users.test.js
+++ services/api/src/tests/users.test.js
@@ -75,10 +75,197 @@ describe("/users", () => {
             expect(body).toStrictEqual(_.omit(newUser, ["password"]))
         })
 
+        it("should respond 400 if password sent is less than 8 characters", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+
+            // Less than 8 chars
+            let result = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "pass",
+                    passwordConfirmation: "pass",
+                    role: "user",
+                })
+
+            expect(result.status).toBe(400)
+            expect(result.body).toHaveProperty("error")
+
+            // More than 32 chars
+            result = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password:
+                        "pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp",
+                    passwordConfirmation: "pass",
+                    role: "user",
+                })
+
+            expect(result.status).toBe(400)
+            expect(result.body).toHaveProperty("error")
+
+            // No upper case
+            result = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "password10",
+                    passwordConfirmation: "pass",
+                    role: "user",
+                })
+
+            expect(result.status).toBe(400)
+            expect(result.body).toHaveProperty("error")
+
+            // No lower case
+            result = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "PASSWORD10",
+                    passwordConfirmation: "pass",
+                    role: "user",
+                })
+
+            expect(result.status).toBe(400)
+            expect(result.body).toHaveProperty("error")
+
+            // no numeric
+            result = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "PasswordVerySafe",
+                    passwordConfirmation: "pass",
+                    role: "user",
+                })
+
+            expect(result.status).toBe(400)
+            expect(result.body).toHaveProperty("error")
+        })
+
+        it("should send 400 if password confirmation field is not identical to the password field", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "Password10",
+                    passwordConfirmation: "anotherPassword",
+                    role: "user",
+                })
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("error")
+        })
+
+        it("should send 400 if password field is missing", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    passwordConfirmation: "anotherPassword",
+                    role: "user",
+                })
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("error")
+        })
+
+        it("should send 400 if password confirmation field is missing", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "newUser",
+                    email: newUserEmail,
+                    password: "Password10",
+                    role: "user",
+                })
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("error")
+        })
+
+        it("should send 400 if name field is missing", async () => {
+            const newUserEmail = "newUser@email.com"
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    email: newUserEmail,
+                    password: "Password10",
+                    passwordConfirmation: "Password10",
+                    role: "user",
+                })
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("error")
+        })
+
+        it("should send 400 if email field is missing", async () => {
+            const token = generateAccessToken(adminUser)
+            const { status, body } = await api
+                .post(v1Api("/users"))
+                .auth(token, {
+                    type: "bearer",
+                })
+                .send({
+                    name: "username",
+                    password: "Password10",
+                    passwordConfirmation: "Password10",
+                    role: "user",
+                })
+
+            expect(status).toBe(400)
+            expect(body).toHaveProperty("error")
+        })
+
         // eslint-disable-next-line
         it("should respond 401 if the authorization header is invalid or missing", async () =>
             await testAuthenticationHeader("/users", adminUser))
 
+        // eslint-disable-next-line
         it("should respond 403 if the user do not have the correct permissions", async () => {
             await testPermissions("/users", regularUser)
         })
