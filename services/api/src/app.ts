8cec1db8dc5460c6a7c3fdde5b0dc75af562b953
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-12-11 18:03:33 +0100

    Add env variables validation via zod

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv"
-config({
-    path: [`.env.${process.env.NODE_ENV}`, ".env"],
-})
+import "./config/env.js"
 import cookieParser from "cookie-parser"
 import express from "express"
 import helmet from "helmet"
