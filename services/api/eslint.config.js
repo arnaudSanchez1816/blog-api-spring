bba8bb79a267d218ada030f4c8a3ce5d22085a01
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-12-11 16:26:04 +0100

    Add ts eslint config

--- services/api/eslint.config.js
+++ services/api/eslint.config.js
@@ -1,9 +1,11 @@
 import { config } from "@repo/eslint-config/base"
 import { defineConfig } from "vitest/config"
 import vitest from "@vitest/eslint-plugin"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
     ...config,
+    tseslint.configs.recommended,
     {
         files: ["src/**/*.test.js"],
         plugins: {
