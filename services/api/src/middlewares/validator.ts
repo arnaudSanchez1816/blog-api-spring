c820526a4b07c4b545ba02eb6068391b5f3b2626
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-12-12 02:25:20 +0100

    Edit validator middleware to fix type mismatch in some cases

--- services/api/src/middlewares/validator.ts
+++ services/api/src/middlewares/validator.ts
@@ -1,5 +1,5 @@
 import z, { ZodError } from "zod"
-import type { ZodObject, ZodRawShape } from "zod"
+import type { ZodObject } from "zod"
 import _ from "lodash"
 import { ValidationError } from "../lib/errors.js"
 import type { Request, Response, NextFunction } from "express"
@@ -12,25 +12,16 @@ interface ZodRequestObject extends ZodObject<z.ZodRawShape> {
     }
 }
 
-export const validateRequest =
-    <
-        SParams extends z.ZodObject<z.ZodRawShape> | undefined,
-        SBody extends z.ZodObject<z.ZodRawShape> | undefined,
-        SQuery extends z.ZodObject<z.ZodRawShape> | undefined,
-        // The ternary statement here handles the possibility of the S[Type] being undefined
-        TParams extends z.infer<
-            SParams extends z.ZodObject<z.ZodRawShape> ? SParams : any
-        >,
-        TQuery extends z.infer<
-            SQuery extends z.ZodObject<z.ZodRawShape> ? SQuery : any
-        >,
-        TBody extends z.infer<
-            SBody extends z.ZodObject<z.ZodRawShape> ? SBody : any
-        >,
-    >(
-        validator: ZodRequestObject
-    ) =>
-    async (
+export const validateRequest = <V extends ZodRequestObject>(validator: V) => {
+    type SParams = V["shape"] extends { params: infer P } ? P : undefined
+    type SBody = V["shape"] extends { body: infer B } ? B : undefined
+    type SQuery = V["shape"] extends { query: infer Q } ? Q : undefined
+
+    type TParams = SParams extends z.ZodTypeAny ? z.infer<SParams> : any
+    type TBody = SBody extends z.ZodTypeAny ? z.infer<SBody> : any
+    type TQuery = SQuery extends z.ZodTypeAny ? z.infer<SQuery> : any
+
+    return async (
         req: Request<TParams, any, TBody, TQuery, any>,
         res: Response,
         next: NextFunction
@@ -44,7 +35,10 @@ export const validateRequest =
 
             _.merge(req.params, data.params)
             _.merge(req.body, data.body)
-            updateQuery(req, _.merge(req.query, data.query))
+            updateQuery(
+                req,
+                _.merge(req.query, data.query) as Record<string, any>
+            )
 
             return next()
         } catch (error) {
@@ -69,13 +63,11 @@ export const validateRequest =
             throw error
         }
     }
+}
 
 // This only modifies req.query when it must change due to validation.
 // `req.query` remains immutable after changing it here.
-function updateQuery<T extends z.infer<ZodRequestObject>>(
-    req: Request<T>,
-    value: Record<string, any>
-) {
+function updateQuery(req: any, value: Record<string, any>) {
     Object.defineProperty(req, "query", {
         ...Object.getOwnPropertyDescriptor(req, "query"),
         writable: false,
