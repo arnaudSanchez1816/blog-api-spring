740600a18bccbae6adb3fde8bafd063c2fb179aa
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-12-29 20:23:21 +0100

    Remove duplicate ts eslint config rules in eslint config

--- services/api/eslint.config.js
+++ services/api/eslint.config.js
@@ -1,11 +1,9 @@
 import { config } from "@repo/eslint-config/base"
 import { defineConfig } from "vitest/config"
 import vitest from "@vitest/eslint-plugin"
-import tseslint from "typescript-eslint"
 
 export default defineConfig([
     ...config,
-    ...tseslint.configs.recommended,
     {
         files: ["src/**/*.test.js", "src/**/*.test.ts"],
         plugins: {
