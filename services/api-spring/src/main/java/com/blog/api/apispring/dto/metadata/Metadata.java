f145149e7d0db854fed392828b48c0713c5e3a76
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-02-03 17:20:31 +0100

    Metadata now contains paging information

--- services/api-spring/src/main/java/com/blog/api/apispring/dto/metadata/Metadata.java
+++ services/api-spring/src/main/java/com/blog/api/apispring/dto/metadata/Metadata.java
@@ -5,16 +5,40 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Metadata
 {
-	private Integer count;
+	private Long count;
+	private Integer page;
+	private Integer pageSize;
 
-	public Metadata count(int count)
+	public Metadata count(long count)
 	{
 		this.count = count;
 		return this;
 	}
 
-	public Integer getCount()
+	public Metadata page(int page)
+	{
+		this.page = page;
+		return this;
+	}
+
+	public Metadata pageSize(int pageSize)
+	{
+		this.pageSize = pageSize;
+		return this;
+	}
+
+	public Long getCount()
 	{
 		return count;
+	}
+
+	public Integer getPage()
+	{
+		return page;
+	}
+
+	public Integer getPageSize()
+	{
+		return pageSize;
 	}
 }
