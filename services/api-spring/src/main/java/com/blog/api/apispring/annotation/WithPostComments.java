fe5c03d77e1613f4f25b1abccdbdee5661bb35a7
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-02-14 22:12:30 +0100

    Add usage comment to a currently unused but useful annotation.

--- services/api-spring/src/main/java/com/blog/api/apispring/annotation/WithPostComments.java
+++ services/api-spring/src/main/java/com/blog/api/apispring/annotation/WithPostComments.java
@@ -1,10 +1,15 @@
 package com.blog.api.apispring.annotation;
 
+import com.blog.api.apispring.converter.PostIdConverter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation used to eagerly fetch comments of a Post inside {@link PostIdConverter}
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WithPostComments
