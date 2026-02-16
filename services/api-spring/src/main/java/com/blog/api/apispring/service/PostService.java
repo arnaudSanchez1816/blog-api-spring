4e8bff9e6ac6421a7b01f743f11ca7f9376140df
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-02-16 19:41:21 +0100

    PostService updatePost now handles correctly markdown.

--- services/api-spring/src/main/java/com/blog/api/apispring/service/PostService.java
+++ services/api-spring/src/main/java/com/blog/api/apispring/service/PostService.java
@@ -8,15 +8,25 @@ import com.blog.api.apispring.repository.PostRepository;
 import com.blog.api.apispring.specs.PostSpecs;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.text.TextContentRenderer;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Safelist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.HtmlUtils;
 
 import java.time.OffsetDateTime;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Slf4j
@@ -110,22 +120,55 @@ public class PostService
 	@Transactional
 	public PostInfoWithAuthorAndTags updatePost(Post post, String title, String body, Set<TagIdOrSlug> tagIdsOrSlugs)
 	{
+		Cleaner cleaner = new Cleaner(Safelist.none());
 		if (body != null)
 		{
-			// TODO : parse body
-			String parsedBody = body;
-			post.setBody(parsedBody);
+			// Update body, description and reading time
+			String sanitizedBody = cleaner.clean(Jsoup.parse(body))
+										  .text();
+			post.setBody(sanitizedBody);
+
+			// Parse markdown to plain text for processing.
+			Parser parser = Parser.builder()
+								  .build();
+			Node parsedBodyMarkdown = parser.parse(body);
+			TextContentRenderer textRenderer = TextContentRenderer.builder()
+																  .build();
+
+			String plainTextBody = textRenderer.render(parsedBodyMarkdown);
+			// Maybe we can only clean the body once?
+			plainTextBody = cleaner.clean(Jsoup.parse(plainTextBody))
+								   .text();
+
+			// Description
+			String description = plainTextBody;
+			// Get first 50 words
+			Pattern pattern = Pattern.compile("(^(?:\\S+\\s*){1,50}).*", Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(description);
+			if (matcher.find())
+			{
+				String first50WordsGroup = matcher.group(1);
+				if (first50WordsGroup != null)
+				{
+					description = first50WordsGroup.trim() + "...";
+				}
+			}
+			post.setDescription(description);
+
+			// Reading time
+			int readingTime = estimateReadingTime(plainTextBody);
+			post.setReadingTime(readingTime);
 		}
 
 		if (title != null)
 		{
-			String parsedTitle = HtmlUtils.htmlEscape(title);
-			post.setTitle(title);
+			String sanitizedTitle = cleaner.clean(Jsoup.parse(title))
+										   .text();
+			post.setTitle(sanitizedTitle);
 		}
 
 		if (tagIdsOrSlugs != null)
 		{
-			// TODO : parse tags
 			Set<Tag> newTags = new HashSet<>();
 			if (!tagIdsOrSlugs.isEmpty())
 			{
@@ -141,20 +184,34 @@ public class PostService
 						ids.add(idOrSlug.getId());
 					}
 				}
+				// todo : is there a way to fetch entity references from a slug ?
 				newTags = tagService.getAllTagsByIdOrSlug(ids, slugs);
 			}
 			post.setTags(newTags);
 		}
 
 		post = postRepository.save(post);
-		// TODO : Check if there is a way to have that projection without a query.
-		Optional<PostInfoWithAuthorAndTags> opUpdatedPost = postRepository.findInfoWithTagsById(post.getId());
-		if (opUpdatedPost.isEmpty())
+		// Eager fetch tags and author, maybe there is a better way?
+		Set<Tag> postTags = post.getTags();
+		User author = post.getAuthor();
+		// Make a projection from the post.
+		ProjectionFactory pf = new SpelAwareProxyProjectionFactory();
+		PostInfoWithAuthorAndTags postProjection = pf.createProjection(PostInfoWithAuthorAndTags.class, post);
+
+		return postProjection;
+	}
+
+	private int estimateReadingTime(String plainText)
+	{
+		Pattern pattern = Pattern.compile("\\S+");
+		Matcher matcher = pattern.matcher(plainText);
+		int count = 0;
+		while (matcher.find())
 		{
-			throw new RuntimeException("Post is unexpectedly null. This should not happen");
+			count += 1;
 		}
 
-		return opUpdatedPost.get();
+		return count > 0 ? Math.max(count / 200, 1) : 1;
 	}
 
 	@Transactional
